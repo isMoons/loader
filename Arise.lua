d13c2ce3c92fd5e37fb7bb6217553c2cae04de2b
Naaellx <naelprakasa@gmail.com>
2025-03-25 21:46:10 +0700

    Update Arise.lua

--- Arise.lua
+++ Arise.lua
@@ -1,2 +1,3 @@
+    -- [[Naaellx]] all my project will be continued join discord for free open source: https://discord.gg/eTVvgvRq4s
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thantzy/loader/refs/heads/main/ThanHubArise.lua" ))()
 
